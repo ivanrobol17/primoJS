b067b772beee9290b34d571bbe558907b60910ba
ivanrobol17 <122744477+ivanrobol17@users.noreply.github.com>
2023-01-18 17:45:47 +0100

    fine

--- test2.js
+++ test2.js
@@ -2,14 +2,15 @@ let val='#';
 let val2=' ';
 let ris2="";
 for (let i = 1; i <= 100; i++) {
+    ris2+=i;
     if(i%3==0 && i%5==0){
-        ris2+="Fiz & Buzz;\n";
+        ris2+=" --> Fiz & Buzz;\n";
     }else if(i%3==0){
-        ris2+="Fiz;\n";
+        ris2+=" --> Fiz;\n";
     }else if(i%5==0){
-        ris2+="Buzz;\n";
+        ris2+=" --> Buzz;\n";
     }else{
-        ris2+=i+";\n";
+        ris2+=";\n";
     }
 }
 console.log(ris2);
